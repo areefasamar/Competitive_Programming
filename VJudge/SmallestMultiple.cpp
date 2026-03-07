24146e74133222478477eefc334b0b4d19322d82
Areefa Samar <samar4802098@cloud.neduet.edu.pk>
2026-03-08 01:15:22 +0500

    Add URL comment for problem reference

---
+++ VJudge/SmallestMultiple.cpp
@@ -0,0 +1,19 @@
+//URL: https://vjudge.net/contest/794807#problem/C
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+
+        int ans = 1;
+
+        for(int i = 1; i <= n; i++){
+            ans = (ans * i) / __gcd(ans, i);
+        }
+        cout << ans << endl;
+    }
+}
